16a660e4c73c477ac961e118c910c67b360385f6
cornollc <cornollc@gmail.com>
2023-05-04 21:52:40 +0600

    environment okay

--- src/components/Home/Review/Review.jsx
+++ src/components/Home/Review/Review.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import Rating from 'react-rating';
 import { useLoaderData, useParams } from 'react-router-dom';
 
 const Review = () => {
     const reviewData = useLoaderData();
     const reviewDetails = reviewData.recipy;
     console.log(reviewDetails)
+
+    const handleFav = event =>{
+        event.currentTarget.disabled = true;
+    }
 
 
     return (
@@ -16,7 +21,23 @@ const Review = () => {
                         <p className='text-2xl font-semibold'>Recipe Name: {recipys.name}</p>
                         <p className='text-xl font-semibold'>Recipe Number: {recipys.recipy_number}</p>
                         <p className='font-semibold'>experience: {recipys.exprience} (year)</p>
-                        <button className="btn btn-primary my-2">Favorite</button>
+                        <div className='flex justify-between'>
+                            <div>
+                                <h2 className='font-semibold'>INGREDIENTS</h2>
+                                <p>Flour, Oil, Pepper, Onion, Salt</p>
+                            </div>
+                            <div>
+                                <button onClick={handleFav} className="btn btn-primary my-2">Favorite</button>
+                            </div>
+                        </div>
+
+                        <Rating
+                            placeholderRating={recipys.id}
+                            emptySymbol={<img src="assets/images/star-grey.png" 
+                            className="icon" />}
+                            placeholderSymbol={<img src="assets/images/star-red.png" className="icon" />}
+                            fullSymbol={<img src="assets/images/star-yellow.png" className="icon" />}
+                        ></Rating>
                     </div>
                 )
             }
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBD94w8wBrR94SXhM3A90C4d0c6gs7QBAw",
-  authDomain: "the-chef-zone.firebaseapp.com",
-  projectId: "the-chef-zone",
-  storageBucket: "the-chef-zone.appspot.com",
-  messagingSenderId: "553764626106",
-  appId: "1:553764626106:web:778305a145568a12026c83"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
